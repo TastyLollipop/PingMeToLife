e0d9def6be1735700740784de5d0253b766ba5c1
Lollipop <96362005+TastyLollipop@users.noreply.github.com>
2023-04-01 15:03:15 +0200

    Initial commit

---
+++ PingMeToLife/Start.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+
+namespace PingMeToLife
+{
+    public static class Start
+    {
+        [DllImport("kernel32.dll")] static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")] static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public static string address = "www.google.com";
+
+        private static int timeout = 1000;
+
+        private static bool isDebugging = false;
+
+        public static void Main()
+        {
+            if (isDebugging) ShowWindow(GetConsoleWindow(), 5);
+            else ShowWindow(GetConsoleWindow(), 0);
+            Ping();
+        }
+
+        private static void Ping()
+        {
+            Ping pingSender = new Ping();
+
+            while (true)
+            {
+                if (isDebugging)
+                {
+                    PingReply sender = pingSender.Send(address, timeout);
+                    Console.WriteLine($"{sender.Address} / {sender.RoundtripTime}");
+                }
+                else pingSender.Send(address, timeout);
+
+                Thread.Sleep(timeout);   
+            }
+        }
+    }
+}
